0ce46882f137eee398a90b21a3bba9a0a3b158bc
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2018-04-11 18:23:08 -0700

    add 3rd question to fb_examples

--- fb_examples.sql
+++ fb_examples.sql
@@ -65,3 +65,33 @@ JOIN friends AS u2_friends
 ON u1_friends.friend_id = u2_friends.friend_id
 WHERE u1_friends.user_id = 42
 AND u2_friends.user_id = 1411
+
+-- 3. Suppose you have 4 tables:
+-- users      friends     events    attendance
+-- -----      -------     ------    ----------
+-- id         user1_id    id        event_id
+-- name       user2_id    host_id   user_id
+--                        date
+--
+-- Assume that if A and B are friends, then either (A,B)
+-- or (B,A) appears in the friends table, but not both.
+--
+-- For each user id, find all events  that user's friends attended
+-- within the last 7 days.
+
+SELECT friends.user1_id AS user_id, events.id
+FROM friends JOIN attendance
+ON friends.user2_id = attendance.user_id
+JOIN events
+ON attendance.event_id = events.id
+WHERE events.date >= DATE_SUB(CURRENT_DATE, 7 days)
+
+-- Need union because the friends table is asymmetric -
+UNION
+
+SELECT friends.user2_id AS user_id, events.id
+FROM friends JOIN attendance
+ON friends.user1_id = attendance.user_id
+JOIN events
+ON attendance.event_id = events.id
+WHERE events.date >= DATE_SUB(CURRENT_DATE, 7 days)
